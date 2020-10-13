8855fd9aa35c7caff1c280c2326f5f6eee42ec9c
Jon Zachary <jontzachary@gmail.com>
2020-10-13 16:51:15 -0700

    add css module and new css-module page

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from '../components/container'
+
+export default function AboutCssModules() {
+  return (
+    <div>
+      <Container>
+        <h1>About CSS modules</h1>
+        <p>CSS modules are cool</p>
+      </Container>
+    </div>
+  )
+}
