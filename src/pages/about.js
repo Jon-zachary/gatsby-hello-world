a6fb95b13fed4882487a8073d31cfcde1f7d7bbc
Jon Zachary <jontzachary@gmail.com>
2020-10-13 17:07:09 -0700

    add css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Link } from 'gatsby'
 
 export default function About() {
   return (
-    <div style={{ color: 'teal' }}>
-      <Header headerText = "About Gatsby, now with components" />
-      <Header headerText = "reusing code like a fucking badass" />
+    <div style={{ color: "teal" }}>
+      <Link to="/">Home</Link>
+      <Link to="/contact/">Contact</Link>
+      <Link to="/about-css-modules/">blog post</Link>
+      <Header headerText="About Gatsby, now with components" />
+      <Header headerText="reusing code like a fucking badass" />
       <p>Very React</p>
     </div>
   )
